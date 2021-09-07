4fb14341db775bb0dcb6d8ec4b3322c2178c8eb6
Jasiel Padrón <madjaze.b1@gmail.com>
2021-09-06 21:14:06 -0400

    string template

--- docs/JS/inputRangeBackgroundColor.js
+++ docs/JS/inputRangeBackgroundColor.js
@@ -7,17 +7,17 @@ function changeColor() {
     let red = document.getElementById('rangeRed').value; 
     let green = document.getElementById('rangeGreen').value;
     let blue = document.getElementById('rangeBlue').value;
-
+    console.log(red ,  blue , green)
     // color é a mistura das tres cores: red, green and blue. 
     // color é a variavel que vai se reflexar no fundo 
 
-    let color= 'rgb (' + red + ', ' + green + ' , ' + blue + ')';
+    let color= `rgb(${red}, ${green}, ${blue})` ;
     
     document.body.style.backgroundColor = color;
 
     document.getElementById('colorOutput').innerHTML = ': ' + color;
 
-
+    console.log(color)
 //textos
 
 let title = document.getElementById('title')
@@ -27,7 +27,7 @@ let select = document.getElementById('title_select')
     if(red == 0 && blue == 0 && green == 0) {
         title.style.color = "white"
         select.style.color = "white"
-    } else if (red == 255 && blue == 255) {
+    } else if (red == 255 && blue == 255 && green == 255) {
         title.style.color = 'black'
         select.style.color = 'black'        
     }
